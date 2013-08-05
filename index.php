35e79755d18d39dc30650be775df16bc69e0f518
Roberto <roberto.butti@gmail.com>
2013-08-05 14:42:07 +0200

    routing

--- index.php
+++ index.php
@@ -8,64 +8,18 @@ $f3->config('config.ini');
 
 $f3->route('GET /',
 	function($f3) {
-		$classes=array(
-			'Base'=>
-				array(
-					'hash',
-					'json',
-					'session'
-				),
-			'Cache'=>
-				array(
-					'apc',
-					'memcache',
-					'wincache',
-					'xcache'
-				),
-			'DB\SQL'=>
-				array(
-					'pdo',
-					'pdo_dblib',
-					'pdo_mssql',
-					'pdo_mysql',
-					'pdo_odbc',
-					'pdo_pgsql',
-					'pdo_sqlite',
-					'pdo_sqlsrv'
-				),
-			'DB\Jig'=>
-				array('json'),
-			'DB\Mongo'=>
-				array(
-					'json',
-					'mongo'
-				),
-			'Auth'=>
-				array('ldap','pdo'),
-			'Bcrypt'=>
-				array(
-					'mcrypt',
-					'openssl'
-				),
-			'Image'=>
-				array('gd'),
-			'Lexicon'=>
-				array('iconv'),
-			'SMTP'=>
-				array('openssl'),
-			'Web'=>
-				array('curl','openssl','simplexml'),
-			'Web\Geo'=>
-				array('geoip','json'),
-			'Web\OpenID'=>
-				array('json','simplexml'),
-			'Web\Pingback'=>
-				array('dom','xmlrpc')
-		);
-		$f3->set('classes',$classes);
-		echo View::instance()->render('welcome.htm');
+		$links=array();
+		$f3->set('links',$links);
+		echo View::instance()->render('home.html');
 	}
 );
+
+$f3->route('GET /media/@media',
+    function($f3) {
+        echo $f3->get('PARAMS.media').'';
+    }
+);
+
 
 $f3->route('GET /userref',
 	function() {
